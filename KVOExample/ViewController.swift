f84fc578d8051994f13ae87e27ae04fbab11a324
Marta Boteller <marta.boteller@gmail.com>
2019-01-02 15:55:00 +0100

    Initial Commit

---
+++ KVOExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KVOExample
+//
+//  Created by Laura Llunell on 02/01/2019.
+//  Copyright © 2019 Marta Boteller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
